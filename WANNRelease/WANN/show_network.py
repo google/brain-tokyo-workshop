ae9c1b4b553843e0b26f10fb8697cf370cc60add
iwaharu <haru11sum@gmail.com>
2021-06-18 21:18:42 +0900

    add graphs and modify show_network

--- WANNRelease/WANN/show_network.py
+++ WANNRelease/WANN/show_network.py
@@ -7,17 +7,17 @@ from wann_src import *
 from domain import *
 
 def actNum2str(actId):
-    actDict = {1:('Linear','grey'),
-               2:('Unsigned Step Function','green'),
-               3:('Sin','yellow'),
-               4:('Gausian','orange'),
-               5:('tanh','navy'),
-               6:('Sigmoid unsigned','hotpink'),
-               7:('Inverse','black'),
-               8:('Absolute Value','red'),
-               9:('Relu','skyblue'),
-               10:('Cosine','purple'),
-               11:('Squared','lightseagreen')}
+    actDict = {1:('Linear','lightslategrey'),
+               2:('Unsigned Step Function','tomato'),
+               3:('Sin','orange'),
+               4:('Gausian','gold'),
+               5:('tanh','greenyellow'),
+               6:('Sigmoid unsigned','yellowgreen'),
+               7:('Inverse','turquoise'),
+               8:('Absolute Value','skyblue'),
+               9:('Relu','dodgerblue'),
+               10:('Cosine','mediumslateblue'),
+               11:('Squared','mediumorchid')}
 
     item = actDict.get(actId)
     name, color = item[0], item[1]
@@ -36,27 +36,36 @@ def main(argv):
     input_size = games[hyp['task']].input_size
     output_size = games[hyp['task']].output_size
     in_out_labels = games[hyp['task']].in_out_labels
+    #if not games[hyp['task']].env_name.startswith('Classify'):
     in_out_labels.insert(input_size,'bias')
-
+    
     # Import individual for testing
     wVec, aVec, wKey = importNet(infile)
 
     dim = int(np.sqrt(np.shape(wVec)[0]))
     wVec = np.reshape(wVec,(dim,dim))
+
+    
     
     # aVec:[N*1], wVec:[N**2 * 1]
-    graph = Digraph(engine='dot', format="png")
-    graph.attr("node", shape="circle")
+    graph = Digraph(engine='dot', format="svg")
+    graph.attr(ranksep='1.5')
+    graph.attr('graph', rankdir="LR")
+    graph.attr("node", shape="oval")
+
+    
     for i in range(dim):
         label, color = actNum2str(aVec[i])
-        if i<= input_size:
-            graph.node(str(i), in_out_labels[i],\
-                       style='filled', fillcolor=color, color=color)
-        elif i==dim-output_size:
-            graph.node(str(i), in_out_labels[-1],\
-                       style='filled', fillcolor=color, color=color)
+        if i <= input_size:
+            graph.node(str(i), str(in_out_labels[i]), color=color)
+            
+        elif dim - output_size <= i:
+            j = i-dim+output_size
+            graph.node(str(i), str(in_out_labels[j]), color='black')
+            
         else:
-            graph.node(str(i), label,color=color)
+            graph.node(str(i), label,color=color,\
+            style='filled', fillcolor=color)
             
     (farray, tarray) = np.where(wVec!=0)
     for i in range(len(farray)):
